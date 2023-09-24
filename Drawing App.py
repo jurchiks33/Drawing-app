947bec661fbd08439fba3e8f5b52ae5e774631f7
jurchiks33 <jurisslisans@gmail.com>
2023-09-24 19:12:45 +0200

    adding increase and decrease brush size

--- Drawing App.py
+++ Drawing App.py
@@ -17,6 +17,13 @@ class DrawingApp:
         self.color_palette = tk.Frame(self.master)
         self.color_palette.pack(side=tk.RIGHT)
 
+        self.button_increase_brush = tk.Button(self.color_palette, text="+", command=self.increase_brush)
+        self.button_increase_brush.pack()
+        self.button_decrese_brush = tk.Button(self.color_palette, text="-", command=self.decrease_brush)
+        self.button_decrese_brush.pack()
+
+        self.brush_size = 2
+
         for color in self.colors:
             color_canvas = tk.Canvas(self.color_palette, bg='white', height=30, width=30)
             color_canvas.pack(pady=5)
@@ -36,7 +43,7 @@ class DrawingApp:
     def paint(self, event):
         x, y = event.x, event.y
         if self.old_x and self.old_y:
-            self.canvas.create_line((self.old_x, self.old_y, x, y), width=2, fill=self.selected_color, capstyle=tk.ROUND, smooth=tk.TRUE)
+            self.canvas.create_line((self.old_x, self.old_y, x, y), width=self.brush_size, fill=self.selected_color, capstyle=tk.ROUND, smooth=tk.TRUE)
         self.old_x = x
         self.old_y = y    
 
@@ -49,6 +56,15 @@ class DrawingApp:
 
     def set_color(self, color):
         self.selected_color = color
+    
+    def increase_brush(self):
+        self.brush_size += 1
+
+    def decrease_brush(self):
+        self.brush_size -= 1
+        if self.brush_size < 1:
+            self.brush_size = 1
+
     
 if __name__ == "__main__":
     root = tk.Tk()
