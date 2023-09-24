5cf9c1dc80d7ce881d817d8f8d5668bfac5373ef
jurchiks33 <jurisslisans@gmail.com>
2023-09-24 18:29:42 +0200

    Code update

--- Drawing App.py
+++ Drawing App.py
@@ -15,10 +15,18 @@ class DrawingApp:
         self.button_clear.pack(side=tk.BOTTOM)
 
         self.old_x = None
-        self.olf_y = None
+        self.old_y = None
 
         self.canvas.bind('<B1-Motion>' , self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
 
-        
+    def paint(self, event):
+        x, y = event.x, event.y
+        if self.old_x and self.old_y:
+            self.canvas.create_line((self.old_x, self.old_y, x, y), width=2, capstyle=tk.ROUND, smooth=tk.TRUE)
+        self.old_x = x
+        self.old_y = y    
 
+    def reset(self, event):
+        self.old_x = None
+        self.old_y = None
